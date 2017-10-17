046952f64a17887261b99c72cd7d62c1d37c83d0
Tyka <tykas@emea.roche.com>
2017-10-17 12:29:28 +0200

    POC of Backup - part1 - CommandLineRunner

--- src/main/java/pl/itcg/home/HomeApplication.java
+++ src/main/java/pl/itcg/home/HomeApplication.java
@@ -3,20 +3,61 @@ package pl.itcg.home;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import pl.itcg.home.person.PersonRepository;
 import pl.itcg.home.activity.ActivityRepository;
+import pl.itcg.home.person.PersonRepository;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class HomeApplication {
 
     private static final Logger log = LoggerFactory.getLogger(HomeApplication.class);
 
+    @Value("${pl.itcg.home.truelife.backup.dir}")
+    private String backupsDir;
+
+    @Value("${pl.itcg.home.truelife.backup.active}")
+    private boolean isBackupActive;
+
     public static void main(String[] args) {
         SpringApplication.run(HomeApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner backup() {
+        return (args) -> {
+            if(!isBackupActive){
+                return;
+            }
+
+            boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+            Process process;
+            if (isWindows) {
+                process = Runtime.getRuntime()
+                        .exec(String.format("cmd.exe /c dir %s", backupsDir));
+            } else {
+                process = Runtime.getRuntime()
+                        .exec(String.format("sh -c ls %s", backupsDir));
+            }
+            StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+            Executors.newSingleThreadExecutor().submit(streamGobbler);
+            int exitCode = process.waitFor();
+            assert exitCode == 0;
+
+            log.info("-------------------------------");
+            log.info("--- BACKUP --------------------");
+            log.info("-------------------------------");
+            log.info("- isBackupActive: " + isBackupActive);
+            log.info("- isWindows: " + isWindows);
+            log.info("- backupsDir: " + backupsDir);
+            log.info("- exitCode: " + exitCode);
+            log.info("-------------------------------");
+        };
     }
 
     @Bean
@@ -30,5 +71,6 @@ public class HomeApplication {
             log.info("-------------------------------");
         };
     }
+
 }
 
--- src/main/java/pl/itcg/home/StreamGobbler.java
+++ src/main/java/pl/itcg/home/StreamGobbler.java
@@ -1,4 +1,21 @@
 package pl.itcg.home;
 
-public class StreamGobbler {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
+class StreamGobbler implements Runnable {
+    private InputStream inputStream;
+    private Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+    }
 }
